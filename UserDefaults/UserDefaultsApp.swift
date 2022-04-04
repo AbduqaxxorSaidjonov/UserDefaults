f68d4b6d49389b6a578a556b739ae679bd4212f1
GitHub <abduqahhor2349@gmail.com>
2022-04-05 01:32:09 +0500

    Initial Commit

---
+++ UserDefaults/UserDefaultsApp.swift
@@ -0,0 +1,20 @@
+//
+//  UserDefaultsApp.swift
+//  UserDefaults
+//
+//  Created by Abduqaxxor on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
